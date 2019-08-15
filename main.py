bd26e3cb6c7261156c9fd5e13804315589cfe82f
Artem Sagdiev <sagdiev@gmail.com>
2019-08-15 21:19:24 +0300

    Add tensorflow tflearn

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from bs4 import BeautifulSoup
+from epub_conversion.utils import open_book, convert_epub_to_lines
 from pathlib import Path
+import tflearn
+from tflearn.data_utils import textfile_to_semi_redundant_sequences, random_sequence_from_string
 from tqdm import tqdm
 import urllib.request
-from epub_conversion.utils import open_book, convert_epub_to_lines
-from bs4 import BeautifulSoup
 
 
 BOOKS = [
@@ -17,7 +19,10 @@ BOOKS = [
     'http://static.flibusta.site/converter/get/convert?url=http%3A%2F%2Fflibusta.is%2Fb%2F831db917512b3ffb2f0246271caff7af%2Fdownload&out=epub&md5=831db917512b3ffb2f0246271caff7af',
     ]
 BUILDDIR = Path('build')
-TEXTFILE = BUILDDIR/ 'books.txt'
+TEXTFILE = BUILDDIR / 'books.txt'
+MODEL = BUILDDIR / 'model'
+
+SEQ_MAXLEN = 5
 
 
 def download():
@@ -29,22 +34,44 @@ def download():
         with urllib.request.urlopen(url) as f:
             (BUILDDIR/f'{i}.epub').write_bytes(f.read())
 
-def train():
+
+def model_structure():
     """Train the model."""
 
     if not TEXTFILE.exists():
-    for path in BUILDDIR.glob('*.epub'):
-        book = open_book(path)
-        lines = convert_epub_to_lines(book)
         text = ''
-        for line in lines:
-            soup = BeautifulSoup(line)
-            text += soup.get_text()
-    TEXTFILE
-    breakpoint()
+        for path in BUILDDIR.glob('*.epub'):
+            book = open_book(path)
+            lines = convert_epub_to_lines(book)
+            for line in lines:
+                soup = BeautifulSoup(line)
+                text += soup.get_text()
+        TEXTFILE.write_text(text)
 
-    #x, y, charix = textfile_to_semi_redundant_sequences(TEXTFILE, )
+    x, y, charidx = textfile_to_semi_redundant_sequences(TEXTFILE, seq_maxlen=SEQ_MAXLEN)
+
+    g = tflearn.input_data([None, SEQ_MAXLEN, len(charidx)])
+    g = tflearn.lstm(g, 512, return_seq=True)
+    g = tflearn.dropout(g, 0.5)
+    g = tflearn.lstm(g, 512, return_seq=True)
+    g = tflearn.dropout(g, 0.5)
+    g = tflearn.lstm(g, 512)
+    g = tflearn.dropout(g, 0.5)
+    g = tflearn.fully_connected(g, len(charidx), activation='softmax')
+    g = tflearn.regression(g, optimizer='adam', loss='categorical_crossentropy', learning_rate=0.001)
+    model = tflearn.SequenceGenerator(g, dictionary=charidx, seq_maxlen=SEQ_MAXLEN, clip_gradients=5.0, checkpoint_path=MODEL)
+
+    return model, x, y, charidx
 
 
 if __name__ == '__main__':
-    model = train()
+
+    #download()
+
+    model, x, y, charidx = model_structure()
+    model.fit(x, y, validation_set=0.1, n_epoch=1, batch_size=128)
+
+    seed = random_sequence_from_string('он вытащил партмоне', seq_maxlen=SEQ_MAXLEN)
+    text = model.generate(100, temperature=0.7, seq_seed=seed)
+
+    print(text)
